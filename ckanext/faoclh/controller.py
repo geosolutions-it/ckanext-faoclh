788e28f0ba894d5306e4aaa35d286a4023a44ab6
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-15 12:50:42 +0200

    Implement ediit/list vacabularies by name

--- ckanext/faoclh/controller.py
+++ ckanext/faoclh/controller.py
@@ -13,16 +13,20 @@ class VocabularyController(base.BaseController):
         }
 
         vocab_type = kwargs['pylons'].request.GET.get('vocab_type', 'fao_resource_type')
+        pk = kwargs['pylons'].request.GET.get('id', None)
         # action = kwargs['pylons'].request.GET.get('action', None)
 
-        vocab_obj = VocabCommand._tags_get({}, vocab_type)
+        vocab_obj = VocabCommand._tags_get({}, pk)
         tags = VocabCommand._vocab_get({}, vocab_type).get('tags', [])
         log.info('log ->> {}'.format(vocab_obj))
 
         context = {
-            'vocal_type': vocab_type,
-            'vocab_obj': vocab_obj,
+            'vocab_type': vocab_type,
+            'vocab_type_name': template_mapper[vocab_type],
             'tags': tags,
             'method': kwargs['pylons'].request.method
         }
-        return base.render('admin/vocabs.html', extra_vars=context)
+
+        if pk:
+            return base.render('admin/edit_create_vocab.html', extra_vars=context)
+        return base.render('admin/list_vocabs.html', extra_vars=context)
