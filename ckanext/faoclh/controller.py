9d20fc1cf7f437ea91016f12ce7359d28a2ac0ac
abulojoshua1 <abulojoshua1@gmail.com>
2020-04-16 02:42:12 +0200

    Add success message alert when tag is created/updated

--- ckanext/faoclh/controller.py
+++ ckanext/faoclh/controller.py
@@ -13,6 +13,7 @@ log = logging.getLogger(__name__)
 
 class VocabularyController(base.BaseController):
     def vocabularies(self, *args, **kwargs):
+        self.created = False
         self.errors = []
         self.template_mapper = {
             u'fao_resource_type': u'Resource Type',
@@ -33,43 +34,44 @@ class VocabularyController(base.BaseController):
 
         if request.method == u'POST':
             self.errors = self.get_package_name_validation_errors(name)
-            log.info('validation err ->> {}: {} '.format(name, self.errors))
             if not self.errors:
-                if self.tag_name != 'NEW':
+                if self.tag_name != u'NEW':
                     self.del_tag({}, self.vocabs, self.tag_name)
 
                 self.add_tag({}, self.vocabs, name)
 
-        log.info('tages ->> {}'.format(self.vocabs))
         context = {
             u'vocab_name': self.vocab_name,
             u'vocab_name_name': self.template_mapper[self.vocab_name],
-            u'tags': self.vocabs.get('tags', []),
+            u'tags': self.vocabs.get(u'tags', []),
             u'method': request.method,
             u'errors': self.errors,
-            u'name': self.tag_name if self.tag_name != 'NEW' else name,
+            u'name': self.tag_name if self.tag_name != u'NEW' else name,
+            u'created': self.created,
         }
 
         if self.tag_name:
+
             return base.render(u'admin/edit_create_vocab.html', extra_vars=context)
         return base.render(u'admin/list_vocabs.html', extra_vars=context)
 
     def add_tag(self, context, vocab, tag_name):
-        vocab_name = vocab['name']
-        data = {'name': tag_name, 'vocabulary_id': vocab['id']}
+        vocab_name = vocab[u'name']
+        data = {u'name': tag_name, u'vocabulary_id': vocab[u'id']}
         try:
-            result = toolkit.get_action('tag_create')(context, data)
-            log.info('success ->> {}'.format(result))
+            result = toolkit.get_action(u'tag_create')(context, data)
+            self.created = True
         except ValidationError as e:
             if u'already belongs to vocabulary' in str(e):
                 self.errors.append(_(u'Vocabulary tag is already existent.'))
+                self.created = False
             return e
         return result
 
     def del_tag(self, context, vocab, tag_name):
-        vocab_name = vocab['name']
-        data = {'id': tag_name, 'vocabulary_id': vocab['id']}
-        return toolkit.get_action('tag_delete')(context, data)
+        vocab_name = vocab[u'name']
+        data = {u'id': tag_name, 'vocabulary_id': vocab[u'id']}
+        return toolkit.get_action(u'tag_delete')(context, data)
 
     def get_package_name_validation_errors(self, package_name):
         context = {u'model': ckan.model, u'session': ckan.model.Session}
@@ -81,13 +83,13 @@ class VocabularyController(base.BaseController):
 
     def get_vocab(self, context, vocab_name):
         try:
-            data = {'id': vocab_name}
-            return toolkit.get_action('vocabulary_show')(context, data)
+            data = {u'id': vocab_name}
+            return toolkit.get_action(u'vocabulary_show')(context, data)
 
         except toolkit.ObjectNotFound:
             return {}
 
     def url_errors(self):
-        tags = [tag['name'] for tag in self.vocabs.get('tags', [])]
-        return self.tag_name and (self.tag_name != 'NEW' and self.tag_name not in tags) \
+        tags = [tag[u'name'] for tag in self.vocabs.get(u'tags', [])]
+        return self.tag_name and (self.tag_name != u'NEW' and self.tag_name not in tags) \
             or not self.vocabs
